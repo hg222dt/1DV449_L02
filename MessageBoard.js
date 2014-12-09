8276dc774d544bcd9cb8420aa206da09705a1f51
hg222dt <henrik.gudmundsson@gmail.com>
2014-12-09 14:35:10 +0100

    getMessages körs nu sist inuti init i MessageBoard

--- MessageBoard.js
+++ MessageBoard.js
@@ -63,6 +63,8 @@ var MessageBoard = {
                                                         return false;
                                                     }
                                                 }
+
+            MessageBoard.getMessages();
     
     },
     setHighestMessageId:function(newId) {
--- mess.php
+++ mess.php
@@ -182,15 +182,5 @@ checkUser();
                     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
                     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
                     <script src="MessageBoard.js"></script>
-
-
-                    <!-- This script is running to get the messages -->
-            			<script>
-            				$(document).ready(function() {
-            					MessageBoard.getMessages();
-            				});
-            			</script>
-                        
-
             	</body>
             </html>
